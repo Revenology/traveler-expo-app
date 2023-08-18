04036f8d80f7629c31489b8ba13eb8457b24de5f
Sean <sean@Seans-MacBook-Pro.local>
2023-08-18 21:23:31 +1000

    Added account, trip, event and journey interface for db

--- constants/types.ts
+++ constants/types.ts
@@ -1,7 +1,48 @@
 export interface UserDataProps {
     username: string;
-    name: string;
+    firstName: string;
+    lastName: string;
+    dob: Date;
     email: string;
     password: string;
     phone: number;
+}
+
+export interface AccountProps extends UserDataProps {
+    id: number;
+    preferences?: string;
+    startData: Date;
+    active: boolean;
+    hobbies?: string;
+}
+
+export interface TripProps {
+    id: number;
+    creatorId: number;
+    journeyId: number;
+    origin: string;
+    destination: string;
+    dateStart: Date;
+    dateEnd: Date;
+    transportation: number;
+    experience: string;
+    participants: string;
+}
+
+export interface EventProps {
+    id: number;
+    tripId: number;
+    location: string;
+    date: Date;
+    category: string;
+    description: string;
+}
+
+export interface JourneyProps {
+    id: number;
+    origin: string;
+    length: number;
+    startDate: Date;
+    endDate: Date;
+    lastDestination: string;
 }
