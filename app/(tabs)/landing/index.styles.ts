431d3a10c54a810ca6ef8f0c91af7c7591f10fcb
Sean <sean@Seans-MacBook-Pro.local>
2023-08-18 20:44:17 +1000

    fixed styled components bug

---
+++ app/(tabs)/landing/index.styles.ts
@@ -0,0 +1,34 @@
+import { Text, View } from '@/components/Themed';
+import { styled } from 'styled-components/native';
+
+export const StyledWrapper = styled(View)`
+    height: 100%;
+    flex: 1;
+    align-items: center;
+    justify-content: center;
+`;
+
+export const StyledComponentWrapper = styled(View)`
+    flex: 1;
+    align-items: center;
+    justify-content: center;
+`;
+
+export const Title = styled(Text)`
+    fontsize: 20;
+    fontweight: bold;
+`;
+
+export const StyledLine = styled(View)`
+    margin-top: 50%;
+    color: black;
+    height: 1px;
+    width: 80px;
+`;
+export const StyledImage = styled(View)`
+    align-items: center;
+    justify-content: center;
+    width: 50;
+    height: 50;
+    resizemode: stretch;
+`;
---
+++ app/(tabs)/signup/index.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from '@/components/Themed';
+import React from 'react';
+
+const signup = () => {
+    return (
+        <View>
+            <Text></Text>
+            
+        </View>
+    );
+};
+
+export default signup;
