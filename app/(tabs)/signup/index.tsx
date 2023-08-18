d055d297baf6fe1dd1abb32138ad1ae1bd821916
Sean <sean@Seans-MacBook-Pro.local>
2023-08-18 20:59:49 +1000

    started login struct and interfaces

--- app/(tabs)/signup/index.tsx
+++ app/(tabs)/signup/index.tsx
@@ -1,12 +1,16 @@
 import { Text, View } from '@/components/Themed';
 import React from 'react';
+import { Image } from 'react-native';
+import defaultImage from '../../../assets/images/defaultProfile.png';
+import { StyledWrapper } from './index.styles';
 
 const signup = () => {
     return (
-        <View>
-            <Text></Text>
+        <StyledWrapper>
+            <Image source={defaultImage}/>
+            <Text>This is the signup page</Text>
             
-        </View>
+        </StyledWrapper>
     );
 };
 
---
+++ constants/types.ts
@@ -0,0 +1,7 @@
+export interface UserDataProps {
+    username: string;
+    name: string;
+    email: string;
+    password: string;
+    phone: number;
+}
