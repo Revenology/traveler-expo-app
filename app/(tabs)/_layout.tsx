af12742c907ec19b64be457548f274ba2d834fa1
Sean <sean@Seans-MacBook-Pro.local>
2023-08-24 19:50:32 +1000

    corrected routing issue

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -63,6 +63,13 @@ export default function TabLayout() {
 					tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
 				}}
 			/>
+			<Tabs.Screen
+				name="profile/Index"
+				options={{
+					title: 'Profile',
+					tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+				}}
+			/>
 		</Tabs>
 	);
 }
--- app/(tabs)/profile/Index.tsx
+++ app/(tabs)/profile/Index.tsx
@@ -1,7 +1,12 @@
+import { Text, View } from '@/components/Themed';
 import React from 'react';
 
 const Profile = () => {
-	return <div></div>;
+	return (
+		<View>
+			<Text>This is the profile page</Text>
+		</View>
+	);
 };
 
 export default Profile;
