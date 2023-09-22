134160b8250b33099a5a066bef98d490e85d69b8
Sean <sean@Seans-MacBook-Pro.local>
2023-09-22 22:00:30 +1000

    add openAi connection

---
+++ service/openAi.ts
@@ -0,0 +1,33 @@
+import { OpenAI } from 'openai';
+import { ChatCompletionCreateParams } from 'openai/resources/chat';
+
+const openai = new OpenAI({
+	apiKey: 'APIKey goes here',
+	organization: 'test',
+});
+
+export const countryDataPrompt = (tripData: object): string => {
+	return `Create a trip with the following parameters from this json object and return a json object
+
+    ${tripData.toString()}
+    `;
+};
+
+export const createBody = (tripData: JSON): ChatCompletionCreateParams => {
+	return {
+		stream: true,
+		model: 'gpt-3.5-turbo',
+		messages: [
+			{
+				role: 'system',
+				content: 'You are a virtual assistant responsible for creating plans',
+			},
+			{ role: 'user', content: countryDataPrompt(tripData) },
+		],
+		max_tokens: 10,
+	};
+};
+
+export const response = (tripData: JSON) => {
+	return openai.chat.completions.create(createBody(tripData));
+};
